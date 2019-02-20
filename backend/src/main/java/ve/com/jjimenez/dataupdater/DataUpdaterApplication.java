84b647b12ffc779a93cca9590e1d4a12ec57c19b
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-02-20 10:59:16 -0400

    Base project structure

---
+++ backend/src/main/java/ve/com/jjimenez/dataupdater/DataUpdaterApplication.java
@@ -0,0 +1,13 @@
+package ve.com.jjimenez.dataupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataUpdaterApplication.class, args);
+	}
+
+}
